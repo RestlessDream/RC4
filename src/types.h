016263f6388b2245206ba2f85e4141fd77432df8
Igor Klapatnjuk <mrigorok1998@gmail.com>
2020-10-17 22:52:06 +0300

    Refactoring.

--- src/types.h
+++ src/types.h
@@ -8,10 +8,4 @@
 typedef unsigned char byte;
 typedef uint32_t word;
 
-typedef struct {
-    const size_t key_length;
-    const size_t block_wsize;
-    const size_t rounds;
-} standart_config;
-
 #endif // TYPES_H_
